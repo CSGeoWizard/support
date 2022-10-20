9b71ce2bd1c52a9e17b7ff00b591780d89018c29
CSGeoWizard <thomasclinger1@gmail.com>
2022-10-20 14:23:19 -0400

    append to json and empty the search list

--- tools.py
+++ tools.py
@@ -7,6 +7,24 @@ TITLE = "Update JSON File"
 PROMPT = "What city were you born in?"
 JSON_FILE = "config_test.json"
 JSON_KEY = "city"
+DEFAULT_KWLIST = ["SS-26","bmp","S-400","SS-N-30","TOS-1A","kh-101","kh-102","SA-17","SA-27","SS-NX-33",
+                            "Shahed-136","Shahed-236","Geran-2","Zoopark","rocket","ukraine","elon"]
+
+
+def create_empty_json(json_file):
+    blank_search = []
+
+    file = open(json_file, "r")
+    json_object = json.load(file)
+    file.close()
+
+    # overwrite the current value in the json file with input data
+    file = open(json_file, "w")
+    json_object['twitter']['search_list'] = blank_search
+    json_object['telegram']['search_list'] = blank_search
+    # json_object[json_key] = new_value
+    json.dump(json_object, file, indent=4)
+    file.close()
 
 
 def popup(popup_title, popup_prompt):
@@ -19,20 +37,15 @@ def popup(popup_title, popup_prompt):
     return user_input
 
 
-def modify_twitter_json(json_file, twitter_val=0, keys=['type', 'start_date', 'end_date', 'search', 'n_tweets', 'kwList'], values=[]):
+def modify_twitter_json(json_file, keys=['type', 'start_date', 'end_date', 'search', 'n_tweets', 'kwList'], values=[]):
     """
     json_file: JSON File to modify
     twitter_val: index value in the JSON file
     keys: dictionary containing [type, start_date, end_date, search, n_tweets, kwList]
     values: dictionary containing new values for keys
     """
-
-    # you have to read the file to create the json object and then close it
-    # then open it again in write mode because otherwise the cursor will be
-    # at the end of the first file open that created the json file. Then,
-    # when you try to dump new data into it, it deletes all the data before
-    # the cursor and won't end up writing data because the object field
-    # will be deleted.
+    new_search = {keys[0]: values[0], keys[1]: values[1], keys[2]: values[2], keys[3]: values[3],
+                                  keys[4]: values[4], keys[5]: values[5]}
 
     # open and load json file
     file = open(json_file, "r")
@@ -42,31 +55,19 @@ def modify_twitter_json(json_file, twitter_val=0, keys=['type', 'start_date', 'e
     # overwrite the current value in the json file with input data
     file = open(json_file, "w")
     # json_object[json_key] = new_value
-    for i in range(len(keys)):
-        json_object['twitter']['search_list'][twitter_val][keys[i]] = values[i]
-        json_object['twitter']['search_list'][twitter_val][keys[i]] = values[i]
-        json_object['twitter']['search_list'][twitter_val][keys[i]] = values[i]
-        json_object['twitter']['search_list'][twitter_val][keys[i]] = values[i]
-        json_object['twitter']['search_list'][twitter_val][keys[i]] = values[i]
-        json_object['twitter']['search_list'][twitter_val][keys[i]] = values[i]
+    json_object['twitter']['search_list'].append(new_search)
     json.dump(json_object, file, indent=4)
     file.close()
 
 
-def modify_telegram_json(json_file, twitter_val=0, keys=['channel', 'kwList'], values=[]):
+def modify_telegram_json(json_file, keys=['channel', 'kwList'], values=[]):
     """
     json_file: JSON File to modify
     twitter_val: index value in the JSON file
     keys: dictionary containing [channel, kwList]
     values: dictionary containing new values for keys
     """
-
-    # you have to read the file to create the json object and then close it
-    # then open it again in write mode because otherwise the cursor will be
-    # at the end of the first file open that created the json file. Then,
-    # when you try to dump new data into it, it deletes all the data before
-    # the cursor and won't end up writing data because the object field
-    # will be deleted.
+    new_search = {keys[0]: values[0], keys[1]: values[1], keys[2]: values[2]}
 
     # open and load json file
     file = open(json_file, "r")
@@ -76,9 +77,7 @@ def modify_telegram_json(json_file, twitter_val=0, keys=['channel', 'kwList'], v
     # overwrite the current value in the json file with input data
     file = open(json_file, "w")
     # json_object[json_key] = new_value
-    for i in range(len(keys)):
-        json_object['telegram']['search_list'][twitter_val][keys[i]] = values[i]
-        json_object['telegram']['search_list'][twitter_val][keys[i]] = values[i]
+    json_object['telegram']['search_list'].append(new_search)
     json.dump(json_object, file, indent=4)
     file.close()
 
